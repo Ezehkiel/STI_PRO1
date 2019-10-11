492c7869ff96e481de584ffb14adc59c6a49ee9e
Rémi <mrccrmcc96@gmail.com>
2019-10-11 16:02:07 +0200

    Comment deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+#
+# usage: ./deploy.sh
+#
+# This script is used to deploy the container for the STI Project.
+
+# Clean instance
 docker stop sti_project
 docker rm sti_project
+
+# Start the container
 docker run -p 8080:80 -v "$PWD/site":/usr/share/nginx --env TZ=Europe/Zurich --name sti_project -d -t -i arubinst/sti:project2018 
+
+# Make some commands to start all services and adapt right on database files
 docker exec -u root sti_project service nginx start
 docker exec -u root sti_project service php5-fpm start
 docker exec -u root sti_project sudo chgrp www-data /usr/share/nginx/databases/sti_project.sqlite
